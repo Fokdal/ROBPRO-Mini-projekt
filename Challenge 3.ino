9287e57b83c675deb6bb210036ec67278eed6b6e
Victor Lucassen <144216371+Noerholt@users.noreply.github.com>
2023-12-04 21:45:11 +0100

    Update Challenge 3.ino

--- Challenge 3.ino
+++ Challenge 3.ino
@@ -8,15 +8,26 @@ Zumo32U4IMU imu;
 
 int speed = 220;
 
-uint16_t customBrightnessLevels[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//Define custom brightness levels for increased precision of proximity sensors
+uint16_t customBrightnessLevels[] = {0, 1, 2, 3, 4, 5, 6};
 
 void setup() {
   proxSensors.initThreeSensors();
 
+//set new brightness levels 
   proxSensors.setBrightnessLevels(customBrightnessLevels, sizeof(customBrightnessLevels) / sizeof(customBrightnessLevels[0]));
 
   Serial.begin(9600);
 }
+
+//experimentally found adjustment factor for accuracy
+const float leftSensorAdjustmentFactor = 1.4;
+
+//base motor speeds for motor adjustments
+const int baseMotorSpeed = 60;
+
+//speed adjustment factor for. Amplifies effect of sensor ratios on motor speeds
+const int speedAdjustmentFactor = 4;
 
 void loop() {
     proxSensors.read();
@@ -26,11 +37,16 @@ void loop() {
     Serial.print(proxSensors.countsRightWithRightLeds());
     Serial.println();
 
-    if (proxSensors.countsLeftWithLeftLeds() > proxSensors.countsRightWithRightLeds()) {
-      motors.setSpeeds(speed * int(float((proxSensors.countsLeftWithLeftLeds()) / float(proxSensors.countsRightWithRightLeds()))), 60 + speed * 4 * int(float((proxSensors.countsRightWithRightLeds()) / float(proxSensors.countsLeftWithLeftLeds()/1.4))));
+    int leftSensorCount = proxSensors.countsLeftWithLeftLeds();
+    int rightSensorCount = proxSensors.countsRightWithRightLeds();
+
+    if (leftSensorCount > rightSensorCount) {
+      // If left sensor count is higher, adjust motor speeds accordingly
+      motors.setSpeeds(speed * int(float(leftSensorCount) / float(rightSensorCount)), baseMotorSpeed + speed * speedAdjustmentFactor * int(float((rightSensorCount) / float(leftSensorCount / leftSensorAdjustmentFactor))));
     }
-    else if (proxSensors.countsLeftWithLeftLeds() < proxSensors.countsRightWithRightLeds()) {
-      motors.setSpeeds(60 + speed * 4 * int(float((proxSensors.countsLeftWithLeftLeds()) / float(proxSensors.countsRightWithRightLeds()/1.4))), speed * int(float((proxSensors.countsRightWithRightLeds()) / float(proxSensors.countsLeftWithLeftLeds()))));
+    else if (leftSensorCount < rightSensorCount) {
+      // If right sensor count is higher, adjust motor speeds accordingly
+      motors.setSpeeds(baseMotorSpeed + speed * speedAdjustmentFactor * int(float((leftSensorCount) / float(rightSensorCount / leftSensorAdjustmentFactor))), speed * int(float((rightSensorCount) / leftSensorCount))));
     }
 }
 
